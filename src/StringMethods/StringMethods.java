3ea76adbd932ceba2ec4e5c2352418246c25c270
dylanvanwinkle <dylanvanwinkle@gmail.com>
2021-04-07 11:26:46 -0700

    StringMethods

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -60,29 +60,29 @@ public class StringMethods {
 		String s1New = s1.trim();
 		String s2New = s2.trim();
 		String s3New = s3.trim();
-	int i1 = s1New.indexOf(' ');
-	s1New = s1New.substring(i1);
-	int i2 = s2New.indexOf(' ');
-	s2New = s2New.substring(i2);
-	int i3 = s3New.indexOf(' ');
-	s3New = s3New.substring(i3);
+		int i1 = s1New.indexOf(' ');
+		s1New = s1New.substring(i1);
+		int i2 = s2New.indexOf(' ');
+		s2New = s2New.substring(i2);
+		int i3 = s3New.indexOf(' ');
+		s3New = s3New.substring(i3);
 		int i1New = s1New.compareTo(s2New);
 		if (i1New < 0) {
 			important = s1New.compareTo(s3New);
 			if (important > 0) {
 				return s3.trim();
-			}else if (important < 0) {
+			} else if (important < 0) {
 				return s1.trim();
-			}else {
+			} else {
 				return s3.trim();
 			}
-		}else if (i1New > 0) {
+		} else if (i1New > 0) {
 			important2 = s2New.compareTo(s3New);
 			if (important2 > 0) {
 				return s3.trim();
-			}else if (important2 < 0) {
+			} else if (important2 < 0) {
 				return s2.trim();
-			}else {
+			} else {
 				return s2.trim();
 			}
 		}
@@ -91,44 +91,90 @@ public class StringMethods {
 
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int value = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isDigit(s.charAt(i))) {
+				value = value + Character.getNumericValue(s.charAt(i));
+			}
+		}
+		return value;
 	}
 
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
-		return 0;
+		int counter = 0;
+		int loc = s.indexOf(substring);
+		while (loc != -1) {
+			counter++;
+			loc = s.indexOf(substring, loc + substring.length());
+		}
+
+		return counter;
 	}
 
 	// Call Utitilities.encrypt to encrypt String s
 	public static String encrypt(String s, char key) {
-		return null;
+		byte[] sNew = s.getBytes();
+		String key2 = key + "";
+		byte[] keyNew = key2.getBytes();
+		String message = Utilities.encrypt(sNew, keyNew[0]);
+		return message;
 	}
 
 	// Call Utilities.decrypt to decrypt the cyphertext
 	public static String decrypt(String s, char key) {
-		return null;
+		String key2 = key + "";
+		byte[] keyNew = key2.getBytes();
+		String message = Utilities.decrypt(s, keyNew[0]);
+		return message;
 	}
 
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
-		return 0;
+		int counter = 0;
+		String[] sa = s.split(" ");
+		for (int i = 0; i < sa.length; i++) {
+			if (sa[i].endsWith(substring)) {
+				counter++;
+			}
+		}
+		return counter;
 	}
 
 	// Given String s, return the number of characters between the first occurrence
 	// of String substring and the final occurrence
 	// You can assume that substring will appear at least twice
 	public static int distance(String s, String substring) {
-		return 0;
+		int value = 0;
+		int loc1 = s.indexOf(substring);
+		int loc2 = s.lastIndexOf(substring);
+		loc1 += substring.length();
+		value = loc2 - loc1;
+		return value;
 	}
 
 	// Return true if String s is a palindrome
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
-		return true;
-	}
+		String sNew = "";
+		String sNew2 = "";
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isLetter(s.charAt(i))) {
+				sNew2 += s.charAt(i);
+			}
+			if (Character.isLetter(s.charAt(s.length() - (i + 1)))) {
+				sNew += s.charAt(s.length() - (i + 1));
+			}
+		}
+		
+		if (sNew2.equalsIgnoreCase(sNew)) {
+			return true;
+		}
+		return false;
 
+	}
 }
 
 class Utilities {
--- src/StringMethods/StringMethodsTest.java
+++ src/StringMethods/StringMethodsTest.java
@@ -27,45 +27,45 @@ class StringMethodsTest {
 		assertEquals("Charley F", StringMethods.lineLeader("  Allison Z", " Brad H ", " Charley F "));
 	}
 	
-/*	@Test
+	@Test
 	void testNumeralSum() {
 		assertEquals(0, StringMethods.numeralSum(""));
 		assertEquals(5, StringMethods.numeralSum("11111"));
 		assertEquals(3, StringMethods.numeralSum("a1b2c"));
 		assertEquals(45, StringMethods.numeralSum("x1x2x3x4x5x6x7x8x9x"));
 	}
-*/
-/*	@Test
+
+	@Test
 	void testSubstringCount() {
 		assertEquals(3, StringMethods.substringCount("subsubsub", "sub"));
 		assertEquals(2, StringMethods.substringCount("s ubsubsu bsubs ub", "sub"));
 		assertEquals(3, StringMethods.substringCount("Here I'm counting spaces"," "));
 		assertEquals(0, StringMethods.substringCount("There shoudn't be matches here", "tuna"));
 	}
-	*/
-/*	@Test
+	
+	@Test
 	void testEncrypt() {
 		assertEquals("KwYPDww=", StringMethods.encrypt("Hello", 'c'));
 		assertEquals("FCglYAwlISc1JWAvJmABLSE6KS4nYBAyLycyIS0tJTIz", StringMethods.encrypt("The League of Amazing Programmers", '@'));
 		assertEquals("JBkdFVAEH1AVEQRQAx8dFVAAAhUEChUcAw==", StringMethods.encrypt("Time to eat some pretzels", 'p'));
 	}
-	*/
-/*	@Test
+	
+	@Test
 	void testDecrypt() {
 		assertEquals("Hello", StringMethods.decrypt("KwYPDww=", 'c'));
 		assertEquals("The League of Amazing Programmers", StringMethods.decrypt("FCglYAwlISc1JWAvJmABLSE6KS4nYBAyLycyIS0tJTIz" , '@'));
 		assertEquals("Time to eat some pretzels", StringMethods.decrypt("JBkdFVAEH1AVEQRQAx8dFVAAAhUEChUcAw==", 'p'));
 	}
-	*/
-/*	@Test
+	
+	@Test
 	void testWordsEndWithSubstring() {
 		assertEquals(3, StringMethods.wordsEndsWithSubstring("He quietly and slowly backed away from the bear that was hungrily looking at him", "ly"));
 		assertEquals(2, StringMethods.wordsEndsWithSubstring("He was visiting The League of Amazing Programmers.", "ing"));
 		assertEquals(7, StringMethods.wordsEndsWithSubstring("Here are multiple words that have the same letter at the end.", "e"));
 		assertEquals(0, StringMethods.wordsEndsWithSubstring("This should give us zero matches", "lemonade"));
 	}
-	*/
-/*	@Test
+	
+@Test
 	void testDistance() {
 		assertEquals(6, StringMethods.distance("subsubsubsub", "sub"));
 		assertEquals(7, StringMethods.distance("subsubsubsub", "ub"));
@@ -73,18 +73,17 @@ class StringMethodsTest {
 		assertEquals(0, StringMethods.distance("bb", "b"));
 		
 	}
-	*/
-/*	@Test
+	
+	@Test
 	void testPalindrome() {
 		assertTrue(StringMethods.palindrome("ABA"));
 		assertTrue(StringMethods.palindrome("ABBA"));
 		assertTrue(StringMethods.palindrome("racecar"));
 		assertTrue(StringMethods.palindrome("Was it a cat I saw?"));
-		assertTrue(StringMethods.palindrome("A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal – Panama"));
+		assertTrue(StringMethods.palindrome("A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal Panama"));
 		assertTrue(StringMethods.palindrome("Doc, Note: I Dissent. A Fast Never Prevents A Fatness. I Diet On Cod."));
 		assertFalse(StringMethods.palindrome("abcdefghijklmnopqrstuvwxyz"));
 		assertFalse(StringMethods.palindrome("This is not a palendrome"));
 		assertFalse(StringMethods.palindrome("This is close but not quite right etiuq ton tub esolc si sihT"));
 	}
-	*/
 }
